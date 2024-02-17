dcb377b693123fe80b5f357916258ff41cfa26c6
Maniakndan R <89590224+Leomani2311@users.noreply.github.com>
2024-02-17 16:01:26 +0530

    renamed

---
+++ eCommerce_web/src/page/Fqa.js
@@ -0,0 +1,19 @@
+import  Header  from "../corelayout/Header";
+
+
+export default function Fqa() {
+  return (
+    <div className="main_div">
+      <Header/>
+      <h3>
+        This will add the react-router-dom package to your project's
+        node_modules directory and update your package.json file. Check
+        node_modules: After running the command above, make sure that the
+        node_modules directory contains the react-router-dom folder. If it does,
+        you should see it in the node_modules folder. Check package.json: Open
+        your package.json file and ensure that react-router-dom is listed as a
+        dependency. It should look something like this:
+      </h3>
+    </div>
+  );
+}
