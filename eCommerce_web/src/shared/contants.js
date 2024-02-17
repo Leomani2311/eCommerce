a3b6badc15e1307885ec9d313e1032b4b8655a4f
Maniakndan R <89590224+Leomani2311@users.noreply.github.com>
2024-02-17 17:49:23 +0530

    add a new images

--- eCommerce_web/src/shared/contants.js
+++ eCommerce_web/src/shared/contants.js
@@ -1,33 +1,36 @@
+
+const image_url = "http://localhost:3000/"
+
 const new_product_arrays = [
     {
       title: "laptops",
       description: "data1",
-      image: "image_url1",
+      image: image_url+"images/image 30.png",
     },
     {
       title: "laptops1",
       description: "data2",
-      image: "image_url2",
+      image: image_url+"images/image 27.png",
     },
     {
       title: "laptops2",
       description: "data3",
-      image: "image_url3",
+      image: image_url+"images/image 28.png",
     },
     {
       title: "laptop3",
       description: "data4",
-      image: "image_url4",
+      image: image_url+"images/image 27.png",
     },
     {
       title: "laptop4",
       description: "data5",
-      image: "image_url5",
+      image: image_url+"images/image 27.png",
     },
     {
       title: "laptop5",
       description: "data6",
-      image: "image_url6",
+      image: image_url+"images/image 27.png",
     },
   ];
 
